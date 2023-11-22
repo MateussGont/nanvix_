5d1d1e170a79d34a0899435dad930bca6b905fe0
millenyteixeira <44146885+millenyteixeira@users.noreply.github.com>
2023-11-22 12:18:32 -0300

    correcao semop ubin

--- src/ubin/semop/semop.c
+++ src/ubin/semop/semop.c
@@ -37,6 +37,15 @@
 int id;
 int op;
 
+//verifica se o argumento passado e um numero
+int isNumber(const char *str) {
+    char *endptr;
+    strtol(str, &endptr, 10);  // Tenta converter a string para um número na base 10.
+
+    // Se o ponteiro inicial for igual ao ponteiro a string não é um número.
+    return *endptr == '\0';
+}
+
 /*
  * Prints program version and exits.
  */
@@ -54,7 +63,7 @@ static void version(void)
 /*
  * Prints program usage and exits.
  */
-static void usage(void)
+static void usage(int status)
 {
     printf("Usage: semop [options] <id> <op>\n\n");
     printf("Brief: Creates a semaphore. If already exists return semaphore id.\n\n");
@@ -62,47 +71,69 @@ static void usage(void)
     printf("  --help             Display this information and exit\n");
     printf("  --version          Display program version and exit\n");
 
-    exit(EXIT_SUCCESS);
+    exit(status);
 }
 
 /*
  * Gets number of the semaphore.
  */
 static void getargs(int argc, char *const argv[]) {
-    if (argc != 4) {
+
+   /* if (argc != 4) {
         fprintf(stderr, "Uso: %s <id> <op>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }*/
+
+    // Converte argumentos da linha de comando para inteiros
+
+    for (int i = 1; i < argc; i++)
+    {
+        /* Parse command line argument. */
+        if (!strcmp(argv[i], "--help"))
+        {
+            usage(EXIT_SUCCESS);
+        }
+        else if (!strcmp(argv[i], "--version"))
+        {
+            version();
+        }else if (!isNumber(argv[i])) 
+        {
+            fprintf(stderr, "Error: argument is invalid.\n", argv[i]);
+            exit(EXIT_FAILURE);
+        }
+      
+    }
+
+    id = atoi(argv[1]);
+    op = atoi(argv[2]);
+
+    /* Invalid semaphore id. */
+    if (id < 0 || id  > MAX_SEMAPHORES)
+    {
+        fprintf(stderr, "semop: wrong id for semaphore. Please choose a number between 0 and %d\n", 100);
         exit(EXIT_FAILURE);
     }
 
-    // Converte argumentos da linha de comando para inteiros
-    id = atoi(argv[1]);
-    op = atoi(argv[2]);
-    
-
-    /* Invalid semaphore id. */
-    if (id < 0 || id > 100 || op< 0)
-    {
-        fprintf(stderr, "semctl: wrong id for semaphore. Please choose a number between 0 and %d\n", 100);
+    if(op = 0){
+        fprintf(stderr, "semop: wrong value for operation . Please choose a number less than 0 or bigger than 0 %d\n", 0);
         exit(EXIT_FAILURE);
     }
 }
 
 int main(int argc, char *const argv[]) {
-    id = atoi(argv[1]);
-    op = atoi(argv[2]);
+   
     
-   getargs(argc, argv);
 
+    getargs(argc, argv);
 
-    
     int result = semop(id, op);
 
-     printf("\n %d - retorno kernel ", result);
-
-
-
-
-
+    if (result >= 0){
+        printf("\n %d - retorno kernel ", result);
+        return result;
+    } else{
+        return -1;
+    }
 
 }
     
